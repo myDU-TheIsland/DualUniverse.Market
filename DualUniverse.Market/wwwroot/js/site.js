13fac4b7e7d1df0b5d30d250c6320ed6e13ff97f
Paul Layne <me@thearchitect.io>
2024-11-17 12:26:01 -0500

    added css files

--- DualUniverse.Market/wwwroot/js/site.js
+++ DualUniverse.Market/wwwroot/js/site.js
@@ -1,4 +1,16 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+// Homepage Tab js
+function showTab(tabId) {
+  // Hide all tabs
+  document
+    .querySelectorAll('.tab-content')
+    .forEach((tab) => tab.classList.remove('active-tab'));
+  document
+    .querySelectorAll('.tab-button')
+    .forEach((button) => button.classList.remove('active-tab-button'));
 
-// Write your JavaScript code.
+  // Show the selected tab
+  document.getElementById(tabId).classList.add('active-tab');
+  document
+    .querySelector(`[onclick="showTab('${tabId}')"]`)
+    .classList.add('active-tab-button');
+}
